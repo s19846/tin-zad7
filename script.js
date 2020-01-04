c05a7a2191e607d15373788ba9ef9c0d43414bc6
Jakub Rompca <jakub@arcanelab.io>
2020-01-04 10:48:53 +0100

    kolorowanie

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $('.container > div').click(function (e) {
-    
+    let emptyBlock = $('.pusty').get(0);
+    let clickedBlock = e.target;
+    if (clickedBlock.classList.contains('sasiaduje')) {
+        emptyBlock.style.backgroundColor = 'violet';
+        emptyBlock.classList.add('ready');
+        clickedBlock.classList.add('to-move');
+    }
+
+    if (!clickedBlock.classList.contains('sasiaduje')
+        && !clickedBlock.classList.contains('pusty')) {
+        emptyBlock.style.backgroundColor = 'white';
+    }
+
+    if (clickedBlock.classList.contains('ready')) {
+        
+    }
 });
