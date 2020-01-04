fa6767c3835026a9146edb1dd6605332220c9156
onliner10 <mateuszurban2@gmail.com>
2020-01-04 10:48:22 +0100

    identify neighbours

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const gridWidth = 3;
+const gridHeight = 3;
+const neighbourClass = "sasiaduje";
+
+function applyNeighbourClass(allElements, index) {
+    if(index < 0 || index > (gridWidth * gridHeight - 1)) { return; }
+
+    let current = $(allElements[index]);
+    if(current.hasClass("pusty")) {
+        current.addClass(neighbourClass);
+    }
+}
+
+function identifyNeighbours(e) {
+    let allElements = $(".container > div");
+    allElements.removeClass(neighbourClass);
+
+    let myPosition = allElements.index(e);
+
+    let upIndex = myPosition - gridWidth;
+    let downIndex = myPosition + gridWidth;
+    let rightIndex = myPosition + 1;
+    let leftIndex = myPosition - 1;
+
+    applyNeighbourClass(allElements, upIndex);
+    applyNeighbourClass(allElements, downIndex);
+    applyNeighbourClass(allElements, rightIndex);
+    applyNeighbourClass(allElements, leftIndex);
+}
+
 $(".container > div").click(function(e) {
-    console.log(e.target);
+    identifyNeighbours(e.target);
 });
